a9bc34f3f77bcca6027beff84881f0505c192200
JarvisK <seadynamic8@zoho.com>
2016-12-10 15:39:33 -0500

    Add comment for user config file

--- config/config.exs
+++ config/config.exs
@@ -8,6 +8,7 @@ config :porcelain, driver: Porcelain.Driver.Basic
 config :eltd, 
   default_apps: ["teladoc_framework", "provider", "admin", "member", "client"]
 
+# Location for config file where user overridable (runtime) settings can be set.
 config :eltd, user_config_file: "~/.mix/escripts/config.exs"
 
 # This configuration is loaded before any dependency and is restricted
