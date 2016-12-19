98d9000b0b02fa0231205961bf47ba54674c05c1
Jarvis Kwan <jkwan@teladoc.com>
2016-12-19 08:52:31 -0500

    Bump version number

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Eltd.Mixfile do
     [
       app: :eltd,
       escript: escript_config,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.3",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
